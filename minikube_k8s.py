d53ec3d610f71a750803e4d97be08396b3a1ed0d
Yash Indane <yashindane46@gmail.com>
2021-01-19 12:05:08 +0530

    Update minikube_k8s.py

--- minikube_k8s.py
+++ minikube_k8s.py
@@ -1,4 +1,11 @@
 from subprocess import getstatusoutput as gso
+import datetime
+
+def time():
+
+  t  = str(datetime.datetime.now())    
+  current_time = t.split(' ')[1][:5]
+  print(current_time , end = '  ')
 
 repo_name = '/etc/yum.repos.d/confi.repo'
 baseurl_path = 'baseurl=file:///run/media/root/RHEL-8-0-0-BaseOS-x86_64'
@@ -20,10 +27,14 @@ status = gso(f'echo "[path3]" >> {repo_name}')
 status = gso(f'echo "{baseurl_path}/BaseOS" >> {repo_name}')
 status = gso(f'echo "gpgcheck=0" >> {repo_name}')
 
+time()
 print('yum repos configured!' if status[0]==0 else 'failed to configure yum repos!')
 
 #installing docker community edition
 status = gso('yum install docker-ce --nobest -y')
+
+time()
+print('docker-ce installed successfully' if status[0]==0 else 'failed to install docker-ce')
 status = gso('yum install conntrack-tools -y')
 
 '''
@@ -45,6 +56,8 @@ minimum requirements -
 status = gso('curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64')
 
 status = gso('sudo install minikube-linux-amd64 /usr/local/bin/minikube')
+
+time()
 print('minikube-linux-amd64 successfully installed' if status[0] == 0 else 'failed to install')
 
 '''
@@ -53,6 +66,9 @@ the none driver allows advanced minikube users to skip VM creation, allowing min
 status = gso('minikube config set driver none')
 
 status = gso('minikube start --driver=none --kubernetes-version=v1.20.0 --memory=1800mb ')
+
+time()
+print('minikube started' if status[0]==0 else 'failed to start minikube')
 
 
 '''
@@ -73,11 +89,10 @@ status = gso('chmod +x ./kubectl')
 #move the binary in to your PATH
 status = gso('sudo mv ./kubectl /usr/local/bin/kubectl')
 
+time()
+print('kubectl installed successfully' if status[0]==0 else 'failed to install kubectl')
 
-
-
-
-
+print('enviornment ready' if status[0]==0 else 'failed to build the enviornment')
 
 
 
